6ee48165a93305eb381f272ba7244c22935e6a24
evertonYamata <evertonbssantos1204@gmail.com>
2023-03-03 21:56:06 -0300

    atividade

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import PromptSync from "prompt-sync"
 const prompt = PromptSync()
+import chalk from "chalk"
 
 /*const nome = prompt("digite seu nome: ")
 
-console.log(`ola ${nome}!`)
+console.log(chalk.red(`ola ${nome}!`))
 
-const temp = Number(prompt("digite a temperatura: "))
+/*const temp = Number(prompt("digite a temperatura: "))
 
 if(temp > 38){
     console.log("febre")
@@ -16,15 +17,15 @@ else if(temp < 26){
 
 else{
     console.log("nao ta com febre")
-}*/
-/*
+}
+
 let salario = Number(prompt("digite seu salario: "))
 
 const aumento = Number(prompt("digite a porcentagem de aumento: "))
 
 let novosalario = salario*(1+(aumento/100))
 
-console.log(novosalario)*/
+console.log(novosalario)
 
 const idade = Number(prompt("digite o seu ano de nascimento: "))
 const ano = new Date()
@@ -32,13 +33,31 @@ let verificaco = ano.getFullYear() - idade
 
 console.log(verificaco)
 
-/*if(verificaco < 18){
+if(verificaco < 18){
     console.log("nao pode dirigir")
 }
 else{
     console.log("pode dirigir")
 }*/
 
+const n1 = Number(prompt("digite o primeiro valor: "))
+console.log(chalk.blue(n1))
+
+const n2 = Number(prompt("digite o segundo valor: "))
+console.log(chalk.magenta(n2))
+
+const n3 = Number(prompt("digite o terceiro valor: "))
+console.log(chalk.red(n3))
+
+const n4 = Number(prompt("digite o quarto valor: "))
+console.log(chalk.yellow(n4))
+
+const n5 = Number(prompt("digite o ultimo valor: "))
+console.log(chalk.gray(n5))
+
+let media = (n1+n2+n3+n4+n5)/5
+
+console.log(chalk.bgBlue(`a media é ${media}`))
 
 
 
